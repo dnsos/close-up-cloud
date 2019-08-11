911e5d30492358b555573ff8083cef0c1cd63d18
Timo Hausmann <hello.timohausmann@gmail.com>
2019-08-11 15:49:18 +0200

    sanitizeLabel Fix

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,6 +2,11 @@ export function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+/**
+ * sanitizeLabel
+ * removes spaces and interpunctation from a string
+ * @param {String} label Label Name
+ */
 export function sanitizeLabel(label) {
-	return label.replace(/[\s.,/()]/g, '');
+	return label.replace(/[\s.,/()-]/g, '');
 }
