6642197360156d291e59d8cf65856205c68382e8
Timo Hausmann <hello.timohausmann@gmail.com>
2019-10-10 20:16:22 +0200

    Better Zoom for Trackpad and Safari

--- src/components/VizInput.vue
+++ src/components/VizInput.vue
@@ -23,13 +23,29 @@ export default {
   methods: {
     handleScroll(e) {
       if(this.isTransitioning) return;
+      e.preventDefault();
+
+      if(e.deltaY === 0) return;
+      let deltaZoom = Math.abs(e.deltaY);
+      
+      //on a touchpad or magic mouse, deltaY is around 1-6
+      //event is called way more often
+      if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+        deltaZoom /= 500;
+      //on a default mouse on windows, deltaY is 100
+      //event is called with every wheel step
+      } else {
+        deltaZoom /= 2500;
+      }
 
       let desiredZoom = this.cameraZoom.zoom;
       if( e.deltaY > 0 ) {
-        desiredZoom -= (this.cameraZoom.zoom/8);
+        //zoom out
+        desiredZoom -= deltaZoom;
         desiredZoom = Math.max(desiredZoom, this.cameraMinZoom);
       } else {
-        desiredZoom += (this.cameraZoom.zoom/8);
+        //zoom in
+        desiredZoom += deltaZoom;
         desiredZoom = Math.min(desiredZoom, 1);
       }
 
@@ -77,7 +93,7 @@ export default {
   beforeMount() {
   },
   mounted() {
-    document.addEventListener('wheel', this.handleScroll);
+    document.addEventListener('wheel', this.handleScroll, { passive: false});
     window.addEventListener('blur', this.handlePanEnd);
     document.body.addEventListener('mousemove', this.handleCursorMove);
     document.body.addEventListener('mousedown', this.handlePanStart);
