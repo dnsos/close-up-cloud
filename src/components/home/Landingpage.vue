204e0d15bcff85cb1b267e2e801f0bbda760435a
Christopher Pietsch <cpietsch@gmail.com>
2019-12-04 16:21:44 +0100

    fixed landingpage

--- src/components/home/Landingpage.vue
+++ src/components/home/Landingpage.vue
@@ -2,11 +2,7 @@
   <section class="home__section home__landingpage">
     <div class="section__wrapper section__landingpage">
       <div class="video__wrapper">
-        <video
-          :src="animatedLogo"
-          autoplay="true"
-          muted="true"
-        ></video>
+        <video :src="animatedLogo" autoplay="true" muted="true"></video>
       </div>
       <p>Eine experimentelle Visualisierung zur Erkundung von Bildsammlungen</p>
       <router-link class="button button__viz button--primary" to="/viz" exact>Zur Visualisierung</router-link>
@@ -17,13 +13,13 @@
 
 <script>
 export default {
-  name: 'landingpage',
+  name: "landingpage",
   computed: {
-    animatedLogo: function () {
-      return `${process.env.VUE_APP_URL_LOGOS}/CUC_Logo_AnimationX2.mp4` 
+    animatedLogo: function() {
+      return `${process.env.VUE_APP_URL_LOGOS}/CUC_Logo_AnimationX2.mp4`;
     }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
@@ -42,32 +38,33 @@ export default {
   }
 }
 
-.button__viz, .button__about {
+.button__viz,
+.button__about {
   padding: 1rem 1.5rem 0.875rem;
   color: var(--color-ui-highlighted);
-  margin-bottom: calc(var(--grid-spacing)/2);
+  margin-bottom: calc(var(--grid-spacing) / 2);
 }
 .button__about {
-  margin-left: calc(var(--grid-spacing)/2);
+  margin-left: calc(var(--grid-spacing) / 2);
 }
 
 .video__wrapper {
   position: relative;
-  max-width: 1250pxZ
+  max-width: 1250px;
   margin: 0 auto;
-  padding-top: percentage(150/500); 
+  padding-top: percentage(150/500);
 
   video {
     position: absolute;
     top: 0;
     left: 1.5rem;
-    width: 100%; 
+    width: 100%;
   }
 }
 
 .section__landingpage p {
-	margin-bottom: 3rem;
-	margin-left: -1.5rem;
+  margin-bottom: 3rem;
+  margin-left: -1.5rem;
 }
 //fade in text
 .section__landingpage p,
@@ -81,5 +78,4 @@ html.wf-active {
     opacity: 1;
   }
 }
-
 </style>
